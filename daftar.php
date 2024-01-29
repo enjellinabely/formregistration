8bb4c15defbcc8839eefb82b00a3fed37c0f927a
enjellinabely <157446887+enjellinabely@users.noreply.github.com>
2024-01-29 15:01:53 +0700

    Add files via upload

--- daftar.php
+++ daftar.php
@@ -11,9 +11,21 @@ body{
 }
 .main{
     margin: auto;
-    width: 500px;
+    width: 800px;
     padding:10px;
     background: white;
+}
+.card{
+    display: flex;
+    text-align: center;
+}
+.card1{
+    display: flex;
+    text-align: center;
+}
+.card2{
+    display: flex;
+    text-align: center;
 }
 </style>
     <meta charset="UTF-8">
@@ -27,22 +39,29 @@ body{
 <div class="main">
 <h2 align="center" >Daftar Buku</h2>
 <br>
+<div class="card">
 <tr>
 <td style="padding:5px 10px;">
-<img src="ayah.jpeg" style="height:200px;"/>
-<p>Judul: Ayah<br>
-Karya: Andrea Hirata</p>
+<img src="bukucss.jpeg" style="height:200px;"/>
+<p>Judul: CSS Uncover<br>
+Karya: Andre Pratama</p>
 </td>
 <td style="padding:5px 10px;">
-<img src="sejarah.jpeg" style="height:200px;"/>
-<p>Judul: Sejarah Pendidikan Nasional<br>
-Karya: Muhammad Rifa'i</p>
+<img src="boostrap.jpeg" style="height:200px;"/>
+<p>Judul: Bootstrap Uncover<br>
+Karya: Andre Pratama</p>
 </td>
 <td style="padding:5px 10px;">
 <img src="html.jpeg" style="height:200px;"/>
 <p>Judul: HTML Uncover<br>
 Karya: Andre Pratama</p>
 </td>
+</tr>
+</div>
+<br>
+<br>
+<div class="card1">
+<tr>
 <td style="padding:5px 10px;">
 <img src="kundang.jpg" style="height:200px;"/>
 <p>Judul: Malin Kundang<br>
@@ -57,6 +76,22 @@ Karya: Eka Aryani</p>
 <img src="dilan.jpeg" style="height:200px;"/>
 <p>Judul: Dilan<br>
 Karya: Pidi Baiq</p>
+</td>
+</tr>
+</div>
+<br>
+<br>
+<div class="card2">
+<tr>
+<td style="padding:5px 10px;">
+<img src="ayah.jpeg" style="height:200px;"/>
+<p>Judul: Ayah<br>
+Karya: Andrea Hirata</p>
+</td>
+<td style="padding:5px 10px;">
+<img src="sejarah.jpeg" style="height:200px;"/>
+<p>Judul: Sejarah Pendidikan Nasional<br>
+Karya: Muhammad Rifa'i</p>
 </td>
 </tr>
 </div>
